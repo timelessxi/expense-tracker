b23f06674b9b0b98faa96aca5cee9addb2f0c782
Jamie Fury <jamiefury2@gmail.com>
2023-09-03 23:54:47 -0500

    Setup

---
+++ src/components/Transaction.js
@@ -0,0 +1,9 @@
+export const Transaction = ({ transaction }) => {
+  const sign = transaction.amount < 0 ? '-' : '+';
+  return (
+    <li className={sign === '-' ? 'minus' : 'plus'}>
+      {transaction.text} <span>{sign} ${Math.abs(transaction.amount)}</span>
+      <button className='delete-btn'>x</button>
+    </li>
+  );
+};
